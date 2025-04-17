e3d1b45edb3df1c986a3836916283e0cbf9a0b13
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-04-17 04:11:34 +0200

    feat fr1 & fr4 material vatiance

--- src/geoms/constants.ts
+++ src/geoms/constants.ts
@@ -6,4 +6,5 @@ export const colors = {
   copper: [0.9, 0.6, 0.2],
   fr4Green: [0x05 / 255, 0xa3 / 255, 0x2e / 255],
   fr4GreenSolderWithMask: [0x00 / 255, 0x98 / 255, 0x13 / 255],
+  fr1Copper: [0.8, 0.4, 0.2],
 } satisfies Record<string, RGB>
